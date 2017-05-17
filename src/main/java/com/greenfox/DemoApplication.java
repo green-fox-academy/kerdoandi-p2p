23b26aba69eb5971cc09cae4a05c4c4aa010af25
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-17 12:23:06 +0200

    Create new project

--- src/main/java/com/greenfox/DemoApplication.java
+++ src/main/java/com/greenfox/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeertopeerApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeertopeerApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
