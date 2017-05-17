e63a1b52623785b632ec6a704c5f99431e4723a5
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-17 11:43:29 +0200

    Update build.gradle

--- peertopeer/src/main/java/com/greenfox/controller/PeerToPeerController.java
+++ peertopeer/src/main/java/com/greenfox/controller/PeerToPeerController.java
@@ -1,14 +1,20 @@
 package com.greenfox.controller;
 
+import com.greenfox.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PeerToPeerController {
+  @Autowired
+  MessageRepository messageRepository;
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String getstatus() {
+  public String getstatus(Model model) {
+    model.addAttribute("message", messageRepository.findAll());
   return "index";
   }
 }
