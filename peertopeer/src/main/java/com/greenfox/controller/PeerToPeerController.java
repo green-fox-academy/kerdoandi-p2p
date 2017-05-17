966978894698f14b43d9b6df555be10997895956
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-17 11:52:21 +0200

    Delete database

--- peertopeer/src/main/java/com/greenfox/controller/PeerToPeerController.java
+++ peertopeer/src/main/java/com/greenfox/controller/PeerToPeerController.java
@@ -1,7 +1,5 @@
 package com.greenfox.controller;
 
-import com.greenfox.repository.MessageRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PeerToPeerController {
-  @Autowired
-  MessageRepository messageRepository;
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String getstatus(Model model) {
-    model.addAttribute("message", messageRepository.findAll());
   return "index";
   }
 }
