3172dd5b09f6f79c6952f805d6eb88d6d1ee2d9a
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-17 18:12:03 +0200

    Add update endpoint

--- src/main/java/com/greenfox/controller/MainController.java
+++ src/main/java/com/greenfox/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.User;
 import com.greenfox.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -23,10 +25,17 @@ public class MainController {
     }
   }
 
+  @RequestMapping(value = "/update", method = RequestMethod.PUT)
+  public String changeUserName(Model model, @RequestParam(value = "changed_username") String newName){
+    User user = userRepository.findOne((long)1);
+    user.setName(newName);
+    userRepository.save(user);
+    return "redirect:/";
+  }
+
   @RequestMapping(value = "/list", method = RequestMethod.GET)
   public String listUsers(Model model){
     model.addAttribute("users", userRepository.findAll());
     return "userlist";
   }
-
 }
