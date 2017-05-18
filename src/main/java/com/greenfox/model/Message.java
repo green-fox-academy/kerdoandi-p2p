1fa35b488b255d2cf6433459ae5d2c2ef216fd92
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-18 15:08:28 +0200

    Add timestamp to Message class

--- src/main/java/com/greenfox/model/Message.java
+++ src/main/java/com/greenfox/model/Message.java
@@ -3,6 +3,8 @@ package com.greenfox.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Entity
@@ -11,6 +13,7 @@ public class Message {
   String username;
   String text;
   @Id
+  Timestamp timestamp;
   int id;
 
 
@@ -20,6 +23,7 @@ public class Message {
   public Message(String username, String text) {
     this.username = username;
     this.text = text;
+    timestamp = Timestamp.valueOf(LocalDateTime.now());
     Random random = new Random();
     id = random.nextInt(8999999) + 1000000;
   }
