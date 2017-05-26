f81f7cc51ada2b2126cb5f9d5057620467c3f712
Kerdo Andrea <kerdoandi@gmail.com>
2017-05-26 13:05:39 +0200

    Add test for post

--- src/test/java/com/greenfox/DemoApplicationTests.java
+++ src/test/java/com/greenfox/DemoApplicationTests.java
@@ -1,5 +1,12 @@
 package com.greenfox;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfox.model.Client;
+import com.greenfox.model.Message;
+import com.greenfox.model.MessageWithClientId;
+import com.greenfox.model.User;
+import com.greenfox.repository.MessageRepository;
+import com.greenfox.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +22,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.nio.charset.Charset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -29,19 +37,50 @@ public class DemoApplicationTests {
           Charset.forName("utf8"));
 
   private MockMvc mockMvc;
+  private User user;
+  private Message message;
+  private Client client;
+  private MessageWithClientId mwci;
 
   @Autowired
   private WebApplicationContext webApplicationContext;
 
+  @Autowired
+  MessageRepository messageRepository;
+
+  @Autowired
+  UserRepository userRepository;
+
   @Before
   public void setup() throws Exception {
     this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    this.user = new User("andi");
+    this.client = new Client();
+    client.setId("andi");
+    this.message = new Message(user.getName(), "test");
+    this.mwci = new MessageWithClientId(this.message, this.client);
   }
 
   @Test
   public void testStatusIsOk() throws Exception {
     mockMvc.perform(get("/enter"))
             .andExpect(status().isOk());
+  }
+
+  @Test
+  public void testSendMessage() throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+    String jsoninput = objectMapper.writeValueAsString(mwci);
+
+    System.out.println(jsoninput);
+
+    mockMvc.perform(post("/api/message/receive")
+            .contentType(contentType)
+            .content(jsoninput))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.status").value("ok"));
+
   }
 
 
